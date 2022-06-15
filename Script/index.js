88735810809a94b6e96860f4ffea2a7ef61b3e30
MureedSultan <mureedsultangeni@gmail.com>
2022-06-15 11:31:22 +0500

    Working in slider

---
+++ Script/index.js
@@ -0,0 +1,15 @@
+$("#all").on("click",()=>{
+    console.log("all click")
+})
+$("#gadget").on("click",()=>{
+    console.log("gadget click")
+})
+$("#machines").on("click",()=>{
+    console.log("machines click")
+})
+$("#object").on("click",()=>{
+    console.log("object click")
+})
+$("#marketing").on("click",()=>{
+    console.log("marketing click")
+})
